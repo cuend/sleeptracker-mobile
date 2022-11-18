c11c692e47ef95987f8722ff46f8ad3d5d5cb546
Anthony Martinez <anthomm5@uci.edu>
2022-11-18 15:11:54 -0800

    Injected Sleep Srvice data to Log Sleep page

--- sleeptracker/src/app/log-sleep/log-sleep.page.ts
+++ sleeptracker/src/app/log-sleep/log-sleep.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SleepService } from '../services/sleep.service';
+import { OvernightSleepData } from '../data/overnight-sleep-data';
 
 @Component({
   selector: 'app-log-sleep',
@@ -6,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-sleep.page.scss'],
 })
 export class LogSleepPage implements OnInit {
+  sleepService:SleepService;
 
-  constructor() { }
+  constructor(sleepService:SleepService) { 
+    this.sleepService = sleepService;
+  }
 
   ngOnInit() {
   }
