da08d141ec58c7a6218c5c2b75e4d5cfb01e2132
Anthony Martinez <anthomm5@uci.edu>
2022-11-23 17:20:22 -0800

    Built with capcitor

---
+++ sleeptracker/www/src_app_home_home_module_ts.js
@@ -0,0 +1,159 @@
+(self["webpackChunkapp"] = self["webpackChunkapp"] || []).push([["src_app_home_home_module_ts"],{
+
+/***/ 5089:
+/*!*********************************************!*\
+  !*** ./src/app/home/home-routing.module.ts ***!
+  \*********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "HomePageRoutingModule": () => (/* binding */ HomePageRoutingModule)
+/* harmony export */ });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! tslib */ 4929);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ 2560);
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/router */ 124);
+/* harmony import */ var _home_page__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home.page */ 9460);
+
+
+
+
+const routes = [{
+  path: '',
+  component: _home_page__WEBPACK_IMPORTED_MODULE_0__.HomePage,
+  children: [{
+    path: 'sleepiness',
+    loadChildren: () => __webpack_require__.e(/*! import() */ "common").then(__webpack_require__.bind(__webpack_require__, /*! ../sleepiness/sleepiness.module */ 7534)).then(m => m.SleepinessPageModule)
+  }, {
+    path: 'logSleep',
+    loadChildren: () => __webpack_require__.e(/*! import() */ "common").then(__webpack_require__.bind(__webpack_require__, /*! ../log-sleep/log-sleep.module */ 3456)).then(m => m.LogSleepPageModule)
+  }, {
+    path: 'viewData',
+    loadChildren: () => __webpack_require__.e(/*! import() */ "default-src_app_view-data_view-data_module_ts").then(__webpack_require__.bind(__webpack_require__, /*! ../view-data/view-data.module */ 7163)).then(m => m.ViewDataPageModule)
+  }, {
+    path: '',
+    redirectTo: '/home/logSleep',
+    pathMatch: 'full'
+  }]
+}];
+let HomePageRoutingModule = class HomePageRoutingModule {};
+HomePageRoutingModule = (0,tslib__WEBPACK_IMPORTED_MODULE_1__.__decorate)([(0,_angular_core__WEBPACK_IMPORTED_MODULE_2__.NgModule)({
+  imports: [_angular_router__WEBPACK_IMPORTED_MODULE_3__.RouterModule.forChild(routes)],
+  exports: [_angular_router__WEBPACK_IMPORTED_MODULE_3__.RouterModule]
+})], HomePageRoutingModule);
+
+
+/***/ }),
+
+/***/ 2711:
+/*!*************************************!*\
+  !*** ./src/app/home/home.module.ts ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "HomePageModule": () => (/* binding */ HomePageModule)
+/* harmony export */ });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! tslib */ 4929);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/core */ 2560);
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/common */ 4666);
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! @ionic/angular */ 3819);
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @angular/forms */ 2508);
+/* harmony import */ var _home_page__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home.page */ 9460);
+/* harmony import */ var _home_routing_module__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./home-routing.module */ 5089);
+
+
+
+
+
+
+
+let HomePageModule = class HomePageModule {};
+HomePageModule = (0,tslib__WEBPACK_IMPORTED_MODULE_2__.__decorate)([(0,_angular_core__WEBPACK_IMPORTED_MODULE_3__.NgModule)({
+  imports: [_angular_common__WEBPACK_IMPORTED_MODULE_4__.CommonModule, _angular_forms__WEBPACK_IMPORTED_MODULE_5__.FormsModule, _ionic_angular__WEBPACK_IMPORTED_MODULE_6__.IonicModule, _home_routing_module__WEBPACK_IMPORTED_MODULE_1__.HomePageRoutingModule],
+  declarations: [_home_page__WEBPACK_IMPORTED_MODULE_0__.HomePage]
+})], HomePageModule);
+
+
+/***/ }),
+
+/***/ 9460:
+/*!***********************************!*\
+  !*** ./src/app/home/home.page.ts ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "HomePage": () => (/* binding */ HomePage)
+/* harmony export */ });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! tslib */ 4929);
+/* harmony import */ var _home_page_html_ngResource__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home.page.html?ngResource */ 3853);
+/* harmony import */ var _home_page_scss_ngResource__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./home.page.scss?ngResource */ 1821);
+/* harmony import */ var _home_page_scss_ngResource__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_home_page_scss_ngResource__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/core */ 2560);
+/* harmony import */ var _services_sleep_service__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../services/sleep.service */ 5403);
+
+
+
+
+
+let HomePage = class HomePage {
+  constructor(sleepService) {
+    this.sleepService = sleepService;
+  }
+  ngOnInit() {
+    console.log(this.allSleepData);
+  }
+  /* Ionic doesn't allow bindings to static variables, so this getter can be used instead. */
+  get allSleepData() {
+    return _services_sleep_service__WEBPACK_IMPORTED_MODULE_2__.SleepService.AllSleepData;
+  }
+  static #_ = this.ctorParameters = () => [{
+    type: _services_sleep_service__WEBPACK_IMPORTED_MODULE_2__.SleepService
+  }];
+};
+HomePage = (0,tslib__WEBPACK_IMPORTED_MODULE_3__.__decorate)([(0,_angular_core__WEBPACK_IMPORTED_MODULE_4__.Component)({
+  selector: 'app-home',
+  template: _home_page_html_ngResource__WEBPACK_IMPORTED_MODULE_0__,
+  styles: [(_home_page_scss_ngResource__WEBPACK_IMPORTED_MODULE_1___default())]
+})], HomePage);
+
+
+/***/ }),
+
+/***/ 1821:
+/*!************************************************!*\
+  !*** ./src/app/home/home.page.scss?ngResource ***!
+  \************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+// Imports
+var ___CSS_LOADER_API_SOURCEMAP_IMPORT___ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/sourceMaps.js */ 9579);
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/api.js */ 931);
+var ___CSS_LOADER_EXPORT___ = ___CSS_LOADER_API_IMPORT___(___CSS_LOADER_API_SOURCEMAP_IMPORT___);
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, "#container {\n  text-align: center;\n  position: absolute;\n  left: 0;\n  right: 0;\n  top: 50%;\n  transform: translateY(-50%);\n}\n\n#container strong {\n  font-size: 20px;\n  line-height: 26px;\n}\n\n#container p {\n  font-size: 16px;\n  line-height: 22px;\n  color: #8c8c8c;\n  margin: 0;\n}\n\n#container a {\n  text-decoration: none;\n}", "",{"version":3,"sources":["webpack://./src/app/home/home.page.scss"],"names":[],"mappings":"AAAA;EACE,kBAAA;EAEA,kBAAA;EACA,OAAA;EACA,QAAA;EACA,QAAA;EACA,2BAAA;AAAF;;AAGA;EACE,eAAA;EACA,iBAAA;AAAF;;AAGA;EACE,eAAA;EACA,iBAAA;EAEA,cAAA;EAEA,SAAA;AAFF;;AAKA;EACE,qBAAA;AAFF","sourcesContent":["#container {\n  text-align: center;\n  position: absolute;\n  left: 0;\n  right: 0;\n  top: 50%;\n  transform: translateY(-50%);\n}\n\n#container strong {\n  font-size: 20px;\n  line-height: 26px;\n}\n\n#container p {\n  font-size: 16px;\n  line-height: 22px;\n  color: #8c8c8c;\n  margin: 0;\n}\n\n#container a {\n  text-decoration: none;\n}"],"sourceRoot":""}]);
+// Exports
+module.exports = ___CSS_LOADER_EXPORT___.toString();
+
+
+/***/ }),
+
+/***/ 3853:
+/*!************************************************!*\
+  !*** ./src/app/home/home.page.html?ngResource ***!
+  \************************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = "<ion-header [translucent]=\"true\">\n  <ion-toolbar>\n    <ion-title>\n      Sleep Tracker\n    </ion-title>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content [fullscreen]=\"true\">\n  <ion-tabs>\n\n    <ion-tab-bar slot=\"bottom\">\n\n      <ion-tab-button tab=\"logSleep\">\n        <ion-icon name=\"moon-sharp\"></ion-icon>\n        <ion-label>Log Sleep</ion-label>\n      </ion-tab-button>\n  \n      <ion-tab-button tab=\"sleepiness\">\n        <ion-icon name=\"battery-dead-sharp\"></ion-icon>\n        <ion-label>Track Sleepiness</ion-label>\n      </ion-tab-button>\n  \n      <ion-tab-button tab=\"viewData\">\n        <ion-icon name=\"analytics-sharp\"></ion-icon>\n        <ion-label>View Data</ion-label>\n      </ion-tab-button>\n\n    </ion-tab-bar>\n\n  </ion-tabs>\n</ion-content>\n";
+
+/***/ })
+
+}]);
+//# sourceMappingURL=src_app_home_home_module_ts.js.map
